1e55241fbd9d84ba6cae77da7ddc7d6720c4e4af
jeremy <jeremyninety@gmail.com>
2025-05-05 20:32:34 -0400

    Initial Commit

---
+++ blockStocks/blockStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockStocksApp.swift
+//  blockStocks
+//
+//  Created by Jeremy Barnes-Smith on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct blockStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blockStocksTests/blockStocksTests.swift
@@ -0,0 +1,17 @@
+//
+//  blockStocksTests.swift
+//  blockStocksTests
+//
+//  Created by Jeremy Barnes-Smith on 5/5/25.
+//
+
+import Testing
+@testable import blockStocks
+
+struct blockStocksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
